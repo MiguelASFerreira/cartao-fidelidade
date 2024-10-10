b1672535ef4655c66c0385d0df8f0ec062b83aeb
MiguelASFerreira <migant1306@gmail.com>
2024-10-10 20:34:54 -0300

    configurações webpack e json-server

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// Configuração do dayjs
+import './libs/dayjs.js';
+
+// CSS
+import './styles/global.css';
+import './styles/header.css';
+import './styles/history.css';
+import './styles/loyalty-card.css';
+import './styles/progress.css';
+import './styles/user.css';
+import './styles/index.css';

ac5e5c714f081b51017b12517cc2937d5572bae4
MiguelASFerreira <migant1306@gmail.com>
2024-10-11 14:30:23 -0300

    Adicionando toast success e error

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,7 +1,7 @@
 import Toastify from "toastify-js";
 import "toastify-js/src/toastify.css";
 
-export function toast(text) {
+export function toastSucess(text) {
   Toastify({
     text: text,
     duration: 3000,
@@ -13,6 +13,22 @@ export function toast(text) {
     style: {
       background:
         "linear-gradient(90deg, #2daa6e 0%, #2daaaa 33.33%, #856ccf 66.67%, #d658b3 100%)",
+    },
+  }).showToast();
+}
+
+export function toastError(text) {
+  Toastify({
+    text: text,
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "top",
+    position: "center",
+    stopOnFocus: true,
+    style: {
+      background:
+        "red",
     },
   }).showToast();
 }
